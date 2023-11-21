e0558af0e2aa4b992417a773f2f107dcad2975e3
German Pereira <german.pereira03@gmail.com>
2023-11-20 22:22:33 -0300

    first commit

---
+++ AppCoder/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
+    
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()    
